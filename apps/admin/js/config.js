f6b8b5cbc4c3314a1bc8f94cfd32779b79364be9
raul99-creater <dnjscjf0223@gmail.com>
2026-03-20 00:43:30 +0900

    Delete apps directory

--- apps/admin/js/config.js
+++
@@ -1,4 +0,0 @@
-export const APP_NAME = '귤귤';
-export const SUPABASE_URL = 'https://YOUR_PROJECT_REF.supabase.co';
-export const SUPABASE_PUBLISHABLE_KEY = 'YOUR_SUPABASE_PUBLISHABLE_KEY';
-export const TIMEZONE = 'Asia/Seoul';
